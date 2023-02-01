28e615d8880882621d22164c93eeb877ac23ea5b
Brian Taylor <brian.taylor@optimizely.com>
2023-01-31 23:00:58 -0500

    update text

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello nifty world!")
+    HttpResponse::Ok().body("Hello hotrod world!")
 }
 
 #[post("/echo")]
