e63940d379d2b0876fcca37e889615d4e0dd1464
Brian Taylor <brian.taylor@optimizely.com>
2023-02-05 15:48:16 -0500

    noise

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello broke world!")
+    HttpResponse::Ok().body("Hello noisy world!")
 }
 
 // wish there was a way to auto-register this stuff
