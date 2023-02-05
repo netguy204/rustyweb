6b13395230c51004c97bbda036cd5ec718b64b46
Brian Taylor <brian.taylor@optimizely.com>
2023-02-04 20:54:22 -0500

    support for new expand

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello goofy world!")
+    HttpResponse::Ok().body("Hello pastey world!")
 }
 
 #[post("/echo")]
