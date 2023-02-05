05497160ffbb95924343d3dc1c93785c52738577
Brian Taylor <brian.taylor@optimizely.com>
2023-02-05 15:35:08 -0500

    does deploy branch tracking work?

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello spiffy world!")
+    HttpResponse::Ok().body("Hello testing world!")
 }
 
 // wish there was a way to auto-register this stuff
